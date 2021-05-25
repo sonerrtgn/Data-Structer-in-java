8c80dd492b127af2fe7ec402809a79b3ee21a1db
Soner Tuðan <1sonertugan1@gmail.com>
2021-05-26 02:15:05 +0300

    İlk kodlar yazildi,denendi.
    İlk kodlar

---
+++ node_int.java
@@ -0,0 +1,13 @@
+package Linkedlistandhash;
+
+public class node_int {
+	public int sira;
+	public int deger;
+	public node_int ileri;
+	public node_int(int deger,int sira) {
+		this.sira = sira;
+		this.deger = deger;
+		this.ileri = null;
+	}
+		
+}
